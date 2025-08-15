41d9df18f459a6896963f63971f44075a454a539
Antonio081014 <antonio081014@gmail.com>
2025-08-15 11:20:43 -0700

    Add package supported platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ARadioButtonGroup",
+    platforms: [.iOS(.v13), .macOS(.v14), .tvOS(.v13), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
