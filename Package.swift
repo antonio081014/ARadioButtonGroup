f014fce9f332962c8d124689dd59f8d53d5f9e20
Antonio081014 <antonio081014@gmail.com>
2025-08-15 13:45:01 -0700

    Update package supported platforms.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ARadioButtonGroup",
-    platforms: [.iOS(.v13), .macOS(.v14), .tvOS(.v13), .watchOS(.v10)],
+    platforms: [.iOS(.v15), .macOS(.v14), .tvOS(.v13), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
