de458e3d08a4f2cb5171abbef9db92b5975af358
Antonio081014 <antonio081014@gmail.com>
2025-08-15 21:21:58 -0700

    Update min supported iOS version and tvOS version.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ARadioButtonGroup",
-    platforms: [.iOS(.v15), .macOS(.v14), .tvOS(.v13), .watchOS(.v10)],
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
